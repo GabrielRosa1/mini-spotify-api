1f695525294fdbc7776eb15499d10364fcb94e12
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-03-10 21:08:23 -0300

    chore(initial configs)

---
+++ src/main/java/com/insper/mini_spotify_api/MiniSpotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.insper.mini_spotify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpotifyApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MiniSpotifyApiApplication.class, args);
+	}
+
+}
